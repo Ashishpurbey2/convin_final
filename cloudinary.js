2cc48f941d10dfa7674595965f57070afd6738d5
Ashish kumar <ashishpurbey221806@gmail.com>
2023-03-11 21:22:11 +0530

    update

--- cloudinary.js
+++ cloudinary.js
@@ -1,10 +1,17 @@
 const cloudinary = require('cloudinary');
 
+// cloudinary.config({
+//   cloud_name: "sathya195",
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
 cloudinary.config({
-  cloud_name: "sathya195",
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dvaniudsy",
+  api_key: "384143525133664",
+  api_secret: "ZO3pGgXJqHQEh6HaIP8zdIvsp7s"
 });
+
 
 
 module.exports = {cloudinary};
